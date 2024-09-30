5d448da146f04b7ab736ea7f2561d52658c0b541
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-09-30 10:43:56 -0400

    backend finsh

---
+++ Backend/controllers/noteC.js
@@ -0,0 +1,30 @@
+const Note = require('../models/noteM')
+
+const fetchNotes = async() => {
+    
+}
+
+const fetchNote = async() => {
+
+}
+
+const createNote = async() => {
+
+}
+
+const updateNote = async() => {
+
+}
+
+const deleteNote = async() => {
+    
+}
+
+
+module.exports = {
+    fetchNotes,
+    fetchNote,
+    createNote,
+    updateNote,
+    deleteNote
+}
--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,24 @@ require('dotenv').config()
 const express = require('express')
 const PORT = process.env.port || 3000
 const app = express()
+const connectToDb = require('./db/connectToDb')
+const NoteC = require('./controllers/noteC')
+connectToDb()
+//-----------------------------------------------------------------------------------------------
+
+app.get(`/notes`,NoteC.fetchNotes)
+
+app.post(`/notes`,NoteC.createNote)
+
+app.get(`/notes/:id`,NoteC.fetchNote)
+
+app.put(`/notes/:id`,NoteC.updateNote)
+
+app.delete(`/notes/:id`,NoteC.deleteNote)
 
 
+
+//------------------------------------------------------------------------------------------------
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
